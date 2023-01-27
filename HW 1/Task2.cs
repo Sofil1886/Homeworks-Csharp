38c36f2553e21a7aaa3fabffaa59f6546cb5bd5a
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-28 01:17:43 +0300

    new branch

--- HW 1/Task2.cs
+++ HW 1/Task2.cs
@@ -1,27 +1,5 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-System.Console.WriteLine("Введите два числа:");
 
-int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-int max = 0;
-int min = 0;
-
-if (numberA > numberB)
-{
-    max = numberA;
-    min = numberB;
-    System.Console.WriteLine($"max = {max}");
-    System.Console.WriteLine($"min = {min}");
-}
-else if (numberB > numberA)
-{
-    max = numberB;
-    min = numberA;
-    System.Console.WriteLine($"max = {max}");
-    System.Console.WriteLine($"min = {min}");
-}
-else
-{
-    System.Console.WriteLine("Числа равны");
-}
+5 -> 2, 4
+8 -> 2, 4, 6, 8
