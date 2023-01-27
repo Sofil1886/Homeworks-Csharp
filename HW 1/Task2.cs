39dc6646b583538994dd6c0e19bc3c7ac2def3ef
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-28 01:09:58 +0300

    solved 4 Task

--- HW 1/Task2.cs
+++ HW 1/Task2.cs
@@ -1,96 +1,63 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-System.Console.WriteLine("Введите два числа:");
+System.Console.WriteLine("Введите три числа:");
 
 int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
 int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int numberС = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
 int max = 0;
-int min = 0;
 
 if (numberA > numberB)
 {
     max = numberA;
-    min = numberB;
-    System.Console.WriteLine($"max = {max}");
-    System.Console.WriteLine($"min = {min}");
+    if (max > numberС)
+    {
+        System.Console.WriteLine($"max = {max}");
+    }
+    else if (max < numberС)
+    {
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+    }
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+
+
 }
 else if (numberB > numberA)
 {
     max = numberB;
-    min = numberA;
-    System.Console.WriteLine($"max = {max}");
-    System.Console.WriteLine($"min = {min}");
+    if (max > numberС)
+    {
+        System.Console.WriteLine($"max = {max}");
+    }
+    else if (max < numberС)
+    {
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+    }
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+
 }
+
 else
 {
-    System.Console.WriteLine("Числа равны");
+    if (numberС > numberA)
+    {
+
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+
+    }
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+
 }
-
-
-
-// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-// System.Console.WriteLine("Введите три числа:");
-
-// int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-// int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-// int numberС = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-// int max = 0;
-// int min = 0;
-
-// if (numberA > numberB)
-// {
-//     max = numberA;
-//     if (max > numberС)
-//     {
-//         System.Console.WriteLine($"max = {max}");
-//     }
-//     else if (max < numberС)
-//     {
-//         max = numberС;
-//         System.Console.WriteLine($"max = {max}");
-//     }
-
-//     else
-//     {
-//         System.Console.WriteLine("Одного Максимального числа нет");
-//     }
-
-// }
-// else if (numberB > numberA)
-// {
-//     max = numberB;
-//     if (max > numberС)
-//     {
-//         System.Console.WriteLine($"max = {max}");
-//     }
-//     else if (max < numberС)
-//     {
-//         max = numberС;
-//         System.Console.WriteLine($"max = {max}");
-//     }
-
-//     else
-//     {
-//         System.Console.WriteLine("Одного Максимального числа нет");
-//     }
-// }
-// else
-// {
-//         max = numberB;
-//     if (max > numberС)
-//     {
-//         System.Console.WriteLine($"max = {max}");
-//     }
-//     else if (max < numberС)
-//     {
-//         max = numberС;
-//         System.Console.WriteLine($"max = {max}");
-//     }
-
-//     else
-//     {
-//         System.Console.WriteLine("Одного Максимального числа нет");
-//     }
-// }
 
