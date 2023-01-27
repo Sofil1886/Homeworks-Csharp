0464fd4cb8376190ec45026845c8d07db6014233
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-28 00:39:43 +0300

    update

---
+++ HW 1/HW1.cs
@@ -0,0 +1,36 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Введите два числа:");
+
+int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int max = 0;
+int min = 0;
+
+if (numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+    System.Console.WriteLine($"max = {max}");
+    System.Console.WriteLine($"min = {min}");
+}
+else if (numberB > numberA)
+{
+    max = numberB;
+    min = numberA;
+    System.Console.WriteLine($"max = {max}");
+    System.Console.WriteLine($"min = {min}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
